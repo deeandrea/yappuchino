6ebf6303ae012ffda325be6047935e7d86b3d69a
unknown <leobandagarcia@gmail.com>
2026-04-03 01:23:33 +0800

    Unhandled Rejection

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+process.on('unhandledRejection', (err) => {
+    console.error('❌ Unhandled Rejection:', err);
+});
+process.on('uncaughtException', (err) => {
+    console.error('❌ Uncaught Exception:', err);
+});
+
 require('dotenv').config();
 const { Client, GatewayIntentBits, Events, ActivityType, PermissionsBitField, EmbedBuilder } = require('discord.js');
 const express = require("express");
